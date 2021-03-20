5e4d090472d05c653cffb3c170b4a3010b2d0992
rds80 <rippleshah@msn.com>
2021-03-20 15:53:04 -0400

    Working with pipes

--- src/app/productTable.component.ts
+++ src/app/productTable.component.ts
@@ -31,4 +31,10 @@ export class ProductTableComponent {
     dateObject: Date = new Date(2020, 1, 20);
     dateString: string = "2020-02-20T00:00:00.000Z";
     dateNumber: number = 1582156800000;
+
+    selectMap = {
+        "Watersports":"stay dry",
+        "Soccer": "soccer goals",
+        "other": "have fun"
+    }
 }
